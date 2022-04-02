ddb426f55e40dfda9855175ef64e6aba667c9d59
aboongm <aboongm@yahoo.com>
2022-04-02 20:46:15 +0530

    Add gh-pages code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './styles/style.css';
 import test from './module/test-01.js';
 
+var ghpages = require('gh-pages');
+ghpages.publish('dist', function (err) {});
+
 console.log('Hello webpack');
 test();
